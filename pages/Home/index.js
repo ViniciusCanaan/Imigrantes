d8f3b32a65c0c9b54e7e55559a3896bfb7f688f1
ViniciusCanaan <60820733+ViniciusCanaan@users.noreply.github.com>
2021-06-17 18:47:22 -0300

    Ajustes na tela Home

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,23 +1,51 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { View, Text, Button } from 'react-native';
 
-import {AuthContext} from '../../src/contexts/auth';
-
+import { AuthContext } from '../../src/contexts/auth';
+import { Div, PP, Card, CardTitle, CardBody, CardFooter, Container, Head, SubmitButton, SubmitText, Background, TextTitulo, TextoNome, TextoEmail, ImgHome } from './styles';
+import { Platform, TouchableOpacity } from 'react-native';
 
 export default function Home() {
 
   const { user, signOut } = useContext(AuthContext);
 
+  return (
+    <Background>
 
- return (
-   <View>
-       <Text>Home</Text>
-       <Text>{ user && user.nome}</Text>
-       <Text>{ user && user.email}</Text>
-       <Button
-       title="Sair da conta"
-       onPress={() => signOut()}
-       />
-   </View>
+      <Head>
+        <Div>
+          <PP source={require('../../assets/john_doe.png')} />
+        </Div>
+        <Div>
+          <TextoNome> Nome: {user && user.nome}</TextoNome>
+          <TextoEmail> Email: {user && user.email}</TextoEmail>
+        </Div>
+      </Head>
+      <Container>
+        <Card>
+          <CardTitle>
+            <TextTitulo>Cientista de Dados</TextTitulo>
+          </CardTitle>
+          <CardBody>
+            <Text>
+              Olaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </Text>           
+          </CardBody>        
+          <CardFooter>      
+            <SubmitButton>
+              <SubmitText>Cadastrar</SubmitText>
+            </SubmitButton>
+            
+          </CardFooter>
+        </Card>
+      </Container>
+      <Button
+        title="Sair da conta"
+        onPress={() => signOut()}
+        color='red'
+      />
+    </Background>
+
+
   );
 }
---
+++ pages/Home/styles.js
@@ -0,0 +1,103 @@
+import styled from "styled-components/native";
+
+export const Background = styled.View`
+flex:1;
+background-color: #fff;
+`;
+
+export const Head = styled.View`
+background-color: rgba(189, 193, 198, 0.70);
+margin-top: 28px;
+`;
+
+export const Div = styled.View`
+margin-left: 15px
+`;
+
+export const PP = styled.Image`
+margin: 15px;
+height: 75px;
+width: 75px;
+border-radius: 250px;
+`;
+
+export const Container = styled.View`
+justify-content: center;
+margin-top: 50px
+`;
+
+export const Card = styled.View`
+margin-top: 20px;
+background-color: #00b94a;
+border-radius: 10px;
+padding: 0px 0px 1px 0px;
+`;
+
+export const CardTitle = styled.View`
+
+width: 100%;
+background-color: rgba(189, 193, 198, 0.5);
+padding: 15px;
+border-top-left-radius: 10px;
+border-top-right-radius: 10px;
+color: black;
+text-transform: uppercase;
+font-family: Arial;
+overflow: hidden;
+`;
+
+export const TextTitulo = styled.Text`
+font-size: 18;
+
+`;
+
+export const CardBody = styled.View`
+margin: 15px;
+font-family: Arial;
+`;
+
+export const CardFooter = styled.View`
+width: 100%;
+background-color: #fff;
+padding: 10px;
+border-bottom-left-radius: 10px;
+border-bottom-right-radius: 10px;
+`;
+
+
+export const SubmitButton = styled.TouchableOpacity`
+    align-items: center;
+    justify-content: center;
+    background-color: #00b94a;
+    width:100%;
+    height: 45px;
+    border-radius: 10px;
+    color: white;
+    margin-bottom: 280px;
+`;
+export const SubmitText = styled.Text`
+    font-size: 20px;
+    color: white;
+`;
+
+export const Button = styled.TouchableOpacity`
+margin-top: 150px;
+font-size: 20px;
+    color: white;   
+`;
+
+export const TextoNome = styled.Text`
+font-size: 18;
+
+`;
+
+export const TextoEmail = styled.Text`
+font-size: 18;
+
+`;
+
+export const ImgHome = styled.Image`
+margin-bottom: 15px;
+width: 70%;
+height: 100px
+`;
