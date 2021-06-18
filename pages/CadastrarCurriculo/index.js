6bbb9a7df7d2da044adfc3836202d215c650eda4
ViniciusCanaan <60820733+ViniciusCanaan@users.noreply.github.com>
2021-06-17 21:01:54 -0300

    Migrando Tela Cadastro Curriculo

--- pages/CadastrarCurriculo/index.js
+++ pages/CadastrarCurriculo/index.js
@@ -1,29 +1,35 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
 import { AuthContext } from '../../src/contexts/auth';
+import { useNavigation } from '@react-navigation/native';
 
-import {Background, Header,TextoHeader ,TextoBoasVindas, BoasVindas, ImgBoasVindas, BtnCadastrarCurriculo, BtnText} from '../CadastrarCurriculo/styles'; 
+import { Background, Header, TextoHeader, TextoBoasVindas, BoasVindas, ImgBoasVindas, BtnCadastrarCurriculo, BtnText } from '../CadastrarCurriculo/styles';
 
 export default function CadastrarCurriculo() {
 
-    const { user} = useContext(AuthContext);
- return (
-   <Background>
-       <Header>
-           <TextoHeader>Currículo</TextoHeader>
-       </Header>
-       
-       <BoasVindas>
-       <TextoBoasVindas>Olá {user && user.nome} crie rapidamente seu currículo e comece a se inscrever para as vagas!</TextoBoasVindas>
-       </BoasVindas>
-       <ImgBoasVindas source = {require('../../assets/WelcomeImg.png')}/>
-       <BtnCadastrarCurriculo>
-           <BtnText>Cadastrar agora!</BtnText>
-       </BtnCadastrarCurriculo>
-   </Background>
+    const navigation = useNavigation();
 
-   
+    
+    
+
+    const { user } = useContext(AuthContext);
+    return (
+        <Background>
+            <Header>
+                <TextoHeader>Currículo</TextoHeader>
+            </Header>
+
+            <BoasVindas>
+                <TextoBoasVindas>Olá {user && user.nome} crie rapidamente seu currículo e comece a se inscrever para as vagas!</TextoBoasVindas>
+            </BoasVindas>
+            <ImgBoasVindas source={require('../../assets/WelcomeImg.png')} />
+            <BtnCadastrarCurriculo>
+                <BtnText onPress={() =>navigation.navigate('Home')}>Cadastrar agora!</BtnText>
+            </BtnCadastrarCurriculo>
+        </Background>
 
 
-  );
+
+
+    );
 }
